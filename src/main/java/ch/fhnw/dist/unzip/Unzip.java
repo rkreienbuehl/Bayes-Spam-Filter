841869642e463e41becdac501bf098fb404060e4
Florian Thiévent <florianjeremias@gmail.com>
2019-10-06 12:57:21 +0200

    [fth] Unzip for test files

---
+++ src/main/java/ch/fhnw/dist/unzip/Unzip.java
@@ -0,0 +1,122 @@
+package ch.fhnw.dist.unzip;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.*;
+
+import java.util.Arrays;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+
+/**
+ * Unzip Files from a Folder. Output will be saved to /data in System.getproperty("user.dir").
+ * This is the Current destination where the Java Code is executed. See
+ * <a href="https://docs.oracle.com/javase/tutorial/essential/environment/sysprop.html">Java Sysprop</a>
+ */
+public class Unzip {
+    private final Logger logger = LogManager.getLogger(Unzip.class);
+
+    private String zipPath;
+    private String userHome;
+    private FileInputStream fis;
+    private byte[] readbuffer = new byte[1024];
+    private File dataDir;
+    private File destDir;
+
+    public Unzip(String path) {
+        zipPath = path;
+        userHome = System.getProperty("user.dir");
+        dataDir = new File(System.getProperty("user.dir") + File.separator + "data");
+        destDir = new File(dataDir + File.separator + getUnzipDirName());
+        process();
+    }
+
+    /**
+     * Do a cleanup and then process all Files in the ZIP Folder
+     */
+    private void process() {
+        if (!dataDir.exists()) {
+            dataDir.mkdirs();
+        } else {
+            removeRecursive(dataDir);
+        }
+        doUnzip();
+    }
+
+
+    /**
+     * Unzip the folder and add all files to /data directory in user.dir
+     */
+    private void doUnzip() {
+        logger.info("Starting unzip of " + zipPath + " to " + destDir);
+
+        if (!destDir.exists()) destDir.mkdirs();
+
+        try {
+            fis = new FileInputStream(zipPath);
+            ZipInputStream zis = new ZipInputStream(fis);
+            ZipEntry zipFileEntry = zis.getNextEntry();
+
+            while (zipFileEntry != null) {
+                String fileName = zipFileEntry.getName();
+                File unzipedFile = new File(destDir + File.separator + fileName);
+
+                logger.info("Unzipping to " + unzipedFile.getAbsolutePath());
+
+                new File(unzipedFile.getParent()).mkdirs();
+                FileOutputStream fos = new FileOutputStream(unzipedFile);
+                int len;
+                while ((len = zis.read(readbuffer)) > 0) {
+                    fos.write(readbuffer, 0, len);
+                }
+                fos.close();
+
+                zis.closeEntry();
+                zipFileEntry = zis.getNextEntry();
+
+
+            }
+            zis.closeEntry();
+            zis.close();
+            fis.close();
+
+            logger.info("finished unzipping of " + zipPath);
+
+        } catch (IOException e) {
+            logger.error(e.getMessage() + Arrays.toString(e.getStackTrace()));
+        }
+
+
+    }
+
+    /**
+     * Determine the name for the unzipped Folder
+     * @return Name of the Folder
+     */
+    private String getUnzipDirName() {
+        String[] pathParts = zipPath.split(File.separator);
+        String last = pathParts[pathParts.length - 1];
+        String[] directory = last.split("\\.");
+        logger.info("directory will be " + directory[0]);
+        return directory[0];
+    }
+
+    /**
+     * Remove File Content from a Folder recursively
+     * @param folder - File Path
+     */
+    private void removeRecursive(File folder) {
+        File[] files = folder.listFiles();
+        if (files != null) { //some JVMs return null for empty dirs
+            for (File f : files) {
+                if (f.isDirectory()) {
+                    removeRecursive(f);
+                } else {
+                    f.delete();
+                }
+            }
+        }
+    }
+}
