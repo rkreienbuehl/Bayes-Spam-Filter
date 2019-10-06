084b2bf6f21241f36289363775884fd37462891c
Florian Thiévent <florianjeremias@gmail.com>
2019-10-06 13:08:57 +0200

    [fth] added unzip from classpath files.

--- src/main/java/ch/fhnw/dist/BayesSpamFilter.java
+++ src/main/java/ch/fhnw/dist/BayesSpamFilter.java
@@ -1,4 +1,27 @@
 package ch.fhnw.dist;
 
+import ch.fhnw.dist.unzip.Unzip;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+import java.nio.file.Path;
+
 public class BayesSpamFilter {
+
+    private static final Logger logger = LogManager.getLogger(BayesSpamFilter.class);
+
+    public static void main(String[] args) {
+
+
+        logger.info("starting Bayes-Spam-Filter");
+
+        /**
+         * Load Files for test from classpath ressources
+         */
+        File hamFiles = new File(BayesSpamFilter.class.getClassLoader().getResource("Programmieraufgabe1" + File.separator + "ham-anlern.zip").getFile());
+        new Unzip(hamFiles.toString());
+
+
+    }
 }
