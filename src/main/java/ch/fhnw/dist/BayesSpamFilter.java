78bbbb2ed7cbf32e5dae4b1ab66b58b6ca30f279
Florian Thiévent <florianjeremias@gmail.com>
2019-10-08 16:05:11 +0200

    added Map for words

--- src/main/java/ch/fhnw/dist/BayesSpamFilter.java
+++ src/main/java/ch/fhnw/dist/BayesSpamFilter.java
@@ -1,19 +1,20 @@
 package ch.fhnw.dist;
 
+import ch.fhnw.dist.data.Word;
 import ch.fhnw.dist.unzip.Unzip;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.File;
-import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BayesSpamFilter {
 
     private static final Logger logger = LogManager.getLogger(BayesSpamFilter.class);
+    private static Map<String, Word> words = new HashMap<>();
 
     public static void main(String[] args) {
-
-
         logger.info("starting Bayes-Spam-Filter");
 
         /**
@@ -25,6 +26,8 @@ public class BayesSpamFilter {
 
         File spamFiles = new File(BayesSpamFilter.class.getClassLoader().getResource("Programmieraufgabe1" + File.separator + "spam-anlern.zip").getFile());
         new Unzip(spamFiles.toString());
+
+
 
     }
 }
