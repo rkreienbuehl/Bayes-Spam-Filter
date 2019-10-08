b0864abd0f901e7d375406b8723775e9176a27b3
Florian Thiévent <florianjeremias@gmail.com>
2019-10-08 23:07:15 +0200

    added logger, comments and processing logic

--- src/main/java/ch/fhnw/dist/evaluate/WordProcessor.java
+++ src/main/java/ch/fhnw/dist/evaluate/WordProcessor.java
@@ -1,28 +1,64 @@
 package ch.fhnw.dist.evaluate;
 
 
+import ch.fhnw.dist.filter.SpamFilter;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class WordProcessor {
 
-    private String content;
+    private Logger logger = LogManager.getLogger(WordProcessor.class);
 
-    public void setContent(String content) {
+    private String content;
+    private SpamFilter.SpamOrHam spamOrHam;
+
+    /**
+     * set the content for the WordProcessor and the type of Data
+     *
+     * @param content   Content from Mail as String
+     * @param spamOrHam Type of Content SPAM or HAM from Enum SpamOrHam
+     */
+    public void setContent(String content, SpamFilter.SpamOrHam spamOrHam) {
         this.content = content;
+        this.spamOrHam = spamOrHam;
     }
 
-    public Map<String, Word> processContent(){
+    /**
+     * Process the content of a mail
+     *
+     * @return get a Map of key (the word) as <String> and Word Object <Word>
+     */
+    public Map<String, Word> processContent() {
 
         Map<String, Word> wordlist = new HashMap<>();
 
         String[] splittedContent = this.content.split(" ");
 
-        for( String word: splittedContent ){
-            Word listW = new Word(0,0,0.0,0.0);
-            wordlist.put(word, listW);
+        for (String word : splittedContent) {
+
+            if (wordlist.containsKey(word)) {
+                Word fromList = wordlist.get(word);
+                if (this.spamOrHam == SpamFilter.SpamOrHam.HAM) {
+                    fromList.setHam(fromList.getHam() + 1);
+                } else {
+                    fromList.setSpam(fromList.getSpam() + 1);
+                }
+            } else {
+                if (this.spamOrHam == SpamFilter.SpamOrHam.HAM) {
+                    Word listW = new Word(1, 0, 0.0, 0.0);
+                    wordlist.put(word, listW);
+                } else {
+                    Word listW = new Word(0, 1, 0.0, 0.0);
+                    wordlist.put(word, listW);
+                }
+            }
+
+
         }
 
-        return wordlist ;
+        return wordlist;
     }
 }
