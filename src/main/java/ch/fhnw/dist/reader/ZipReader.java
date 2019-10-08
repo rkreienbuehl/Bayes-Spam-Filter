3ee559cb07322f08e1cb8de9bab0bd0bd1a647d6
Florian Thiévent <florianjeremias@gmail.com>
2019-10-08 23:07:40 +0200

    added logger, comments and processing logic

--- src/main/java/ch/fhnw/dist/reader/ZipReader.java
+++ src/main/java/ch/fhnw/dist/reader/ZipReader.java
@@ -7,12 +7,14 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
+import java.util.Enumeration;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 import java.util.zip.ZipInputStream;
 
 
 /**
- * Unzip Files from a Folder and write Content to an Array of Strings
+ * Read File content from a ZipFolder and write Content to an Array of Strings
  */
 public class ZipReader {
     /**
@@ -50,7 +52,8 @@ public class ZipReader {
      * Unzip the folder and add all files to /data directory in user.dir
      */
     public String[] doRead() {
-        logger.info("Starting unzip");
+        String filename = fileNameFromZipPath(zipPath);
+        logger.info("Starting to read from zipfile " + filename);
 
 
         try {
@@ -58,26 +61,24 @@ public class ZipReader {
             ZipInputStream zis = new ZipInputStream(fis, StandardCharsets.UTF_8);
             ZipEntry zipFileEntry = zis.getNextEntry();
 
+
+            logger.info("Num entries in Zip File " + filename + ": #" + countEntriesInZipFile(zipPath));
+
             int mailcount = 0;
-            String[] mails = new String[(int) zipFileEntry.getSize()];
+            String[] mails = new String[countEntriesInZipFile(zipPath)];
 
             while (zipFileEntry != null) {
 
-
-                logger.info("Unzipping " + zipFileEntry.getName());
-
                 int len;
-
                 StringBuilder sb = new StringBuilder();
-
 
                 while ((len = zis.read(readbuffer)) > 0) {
                     sb.append(new String(readbuffer, 0, len));
                 }
 
-                logger.debug(sb.toString());
+                String unformattedContent = removeFormat(sb.toString());
 
-                mails[mailcount] = sb.toString();
+                mails[mailcount] = unformattedContent;
                 mailcount++;
 
                 zis.closeEntry();
@@ -88,7 +89,7 @@ public class ZipReader {
             zis.close();
             fis.close();
 
-            logger.info("finished unzipping of " + zipPath);
+            logger.info("finished reading contents of " + filename);
 
             return mails;
 
@@ -97,6 +98,77 @@ public class ZipReader {
         }
 
         return new String[0];
+
+    }
+
+    /**
+     * Remove format from String like line break, tabs and replace it with a single whitespace
+     * \\n     remove new line
+     * \\r     remove carriage return
+     * &nbsp;  remove html whitespace
+     * \\.     remove all periods
+     * ,       remove all commas
+     * /       remove all slashes
+     * \\(     remove all opening parentheses
+     * \\)     remove all closing parentheses
+     * <       remove all smaler than
+     * >       remove all greater than
+     * =       remove all equals
+     * \\s{2,} remove all 2 or more white space. This must be at the end to prevent double spaces!
+     *
+     * @param content input formatted content
+     * @return unformatted content
+     */
+    private String removeFormat(String content) {
+        return content
+                .replaceAll("\\n", " ")
+                .replaceAll("\\r", " ")
+                .replaceAll("&nbsp;", " ")
+                .replaceAll("\\.", " ")
+                .replaceAll(",", " ")
+                .replaceAll("/", " ")
+                .replaceAll("\\(", " ")
+                .replaceAll("\\)", " ")
+                .replaceAll("<", " ")
+                .replaceAll(">", " ")
+                .replaceAll("=", " ")
+                .replaceAll("\\s{2,}", " ");
+    }
+
+    /**
+     * Count files contained in a ZipFile
+     *
+     * @param zipPath path to zip file as String
+     * @return number of Files in ZipFile
+     */
+    private int countEntriesInZipFile(String zipPath) {
+        int mailsInZip = 0;
+        try {
+            ZipFile zipFile = new ZipFile(zipPath);
+            Enumeration entries = zipFile.entries();
+
+            while (entries.hasMoreElements()) {
+                entries.nextElement();
+                mailsInZip++;
+            }
+            return mailsInZip;
+        } catch (IOException e) {
+            logger.error(e.getMessage() + Arrays.toString(e.getStackTrace()));
+        }
+
+        return mailsInZip;
+    }
+
+    /**
+     * Get ZipFile name from a zipPath
+     *
+     * @param zipPath the path to zip
+     * @return filename
+     */
+    private String fileNameFromZipPath(String zipPath) {
+
+        String[] splittedZipPath = zipPath.split("/");
+        return splittedZipPath[splittedZipPath.length - 1];
 
     }
 }
