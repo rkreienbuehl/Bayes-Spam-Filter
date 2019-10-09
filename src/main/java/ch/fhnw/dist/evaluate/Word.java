0d880ace1b363242785063225dc4a3ee74013dc3
rogerkreienbuehl <r.kreienbuehl>
2019-10-09 18:07:41 +0200

    added variable minimumValue to easy change minimum that gets set when word is never used in spam or ham

--- src/main/java/ch/fhnw/dist/evaluate/Word.java
+++ src/main/java/ch/fhnw/dist/evaluate/Word.java
@@ -7,6 +7,7 @@ public class Word {
     private String word;
     private int ham = 0;
     private int spam = 0;
+    private double minimumValue = 0.1;
     private double hamProbability;
     private double spamProbability;
 
@@ -31,7 +32,7 @@ public class Word {
     public void addHam(int count) {
         this.ham += count;
         this.hamProbability = this.ham / ((double) this.ham + (double) this.spam);
-        this.spamProbability = (this.spam == 0 ? 0.1 : (this.spam / ((double) this.ham + (double) this.spam)));
+        this.spamProbability = (this.spam == 0 ? this.minimumValue : (this.spam / ((double) this.ham + (double) this.spam)));
     }
 
     public int getSpam() {
@@ -40,7 +41,7 @@ public class Word {
 
     public void addSpam(int count) {
         this.spam += count;
-        this.hamProbability = (this.ham == 0 ? 0.1 : (this.ham / ((double) this.ham + (double) this.spam)));
+        this.hamProbability = (this.ham == 0 ? this.minimumValue : (this.ham / ((double) this.ham + (double) this.spam)));
         this.spamProbability = this.spam / ((double) this.ham + (double) this.spam);
     }
 
