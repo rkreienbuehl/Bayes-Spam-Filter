7ae51389731eafdcb65f2682ea835424536dd75b
rogerkreienbuehl <r.kreienbuehl>
2019-10-12 13:32:52 +0200

    renamed methods for processing with identifier and added new methods without identifier

--- src/main/java/ch/fhnw/dist/evaluate/WordProcessor.java
+++ src/main/java/ch/fhnw/dist/evaluate/WordProcessor.java
@@ -6,6 +6,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 public class WordProcessor {
@@ -15,13 +17,23 @@ public class WordProcessor {
     private String content;
     private SpamFilter.SpamOrHam spamOrHam;
 
+
+    /**
+     * set the content for the WordProcessor
+     *
+     * @param content   Content from Mail as String
+     */
+    public void setContent(String content) {
+        this.content = content;
+    }
+
     /**
      * set the content for the WordProcessor and the type of Data
      *
      * @param content   Content from Mail as String
      * @param spamOrHam Type of Content SPAM or HAM from Enum SpamOrHam
      */
-    public void setContent(String content, SpamFilter.SpamOrHam spamOrHam) {
+    public void setContentWithIdentification(String content, SpamFilter.SpamOrHam spamOrHam) {
         this.content = content;
         this.spamOrHam = spamOrHam;
     }
@@ -31,7 +43,7 @@ public class WordProcessor {
      *
      * @return get a Map of key (the word) as <String> and Word Object <Word>
      */
-    public Map<String, Word> processContent() {
+    public Map<String, Word> processContentWithIdentification() {
 
         Map<String, Word> wordlist = new HashMap<>();
 
@@ -49,6 +61,21 @@ public class WordProcessor {
                 }
             }
         }
+        return wordlist;
+    }
+
+
+    public List<String> processContent() {
+        List<String> wordlist = new LinkedList<>();
+
+        String[] splittedContent = this.content.toLowerCase().split(" ");
+
+        for (String word : splittedContent) {
+            if (!wordlist.contains(word)) {
+                wordlist.add(word);
+            }
+        }
+
         return wordlist;
     }
 }
